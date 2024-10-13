ae22f2dc6ac6658621be01c478ece167a36a4c51
parkSeryu <pjh971212@gmail.com>
2024-10-13 14:51:31 +0900

    주문 목록 검색, 취소

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -42,7 +42,7 @@ public class OrderRepository {
             } else {
                 jpql += " and";
             }
-            jpql += "o.status = :status";
+            jpql += " o.status = :status";
         }
 
         //회원 이름 검색
